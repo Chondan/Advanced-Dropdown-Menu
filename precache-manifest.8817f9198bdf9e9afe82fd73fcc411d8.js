28488d2a01042ea126eb50deac0365d3e0e65f2c
Chondan <chondansusuwan@outlook.com>
2020-09-26 22:38:02 +0700

    Added file for publishing on github page

---
+++ precache-manifest.8817f9198bdf9e9afe82fd73fcc411d8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac968d287d53581049a3a11967e79d59",
+    "url": "/Advanced-Dropdown-Menu/index.html"
+  },
+  {
+    "revision": "91dfa554d1b016010969",
+    "url": "/Advanced-Dropdown-Menu/static/css/main.95e5fa14.chunk.css"
+  },
+  {
+    "revision": "7f18a85be32329e09cdb",
+    "url": "/Advanced-Dropdown-Menu/static/js/2.b9c8feeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Advanced-Dropdown-Menu/static/js/2.b9c8feeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91dfa554d1b016010969",
+    "url": "/Advanced-Dropdown-Menu/static/js/main.32ef581e.chunk.js"
+  },
+  {
+    "revision": "a7d5aab5f8884c93949e",
+    "url": "/Advanced-Dropdown-Menu/static/js/runtime-main.ceb1c919.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/Advanced-Dropdown-Menu/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/Advanced-Dropdown-Menu/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/Advanced-Dropdown-Menu/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/Advanced-Dropdown-Menu/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/Advanced-Dropdown-Menu/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/Advanced-Dropdown-Menu/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/Advanced-Dropdown-Menu/static/media/plus.9311af2e.svg"
+  }
+]);
